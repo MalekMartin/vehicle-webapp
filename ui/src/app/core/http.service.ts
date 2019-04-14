b5e73e30be7ed80ae1b53a3aabbde6dd9452a397
MalekMartin <malek.martin@seznam.cz>
2019-04-14 06:20:34 +0200

    Updated config for prod builds.

--- ui/src/app/core/http.service.ts
+++ ui/src/app/core/http.service.ts
@@ -3,31 +3,31 @@ import { Http, RequestOptionsArgs, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class HttpService {
-
     // private _baseUrl = 'http://localhost/moto';
     // private _baseUrl = 'http://192.168.1.102:4200';
 
-    constructor(private _http:Http, private _auth: AuthService) { }
+    constructor(private _http: Http, private _auth: AuthService) {}
 
     post<IN, OUT>(url: string, body: IN): Observable<OUT> {
         return this._http
-            .post(this.buildUrl(url), JSON.stringify(body), this.createHeaders)
-            .pipe(map((response) => <OUT>response.json()));
+            .post(buildUrl(url), JSON.stringify(body), this.createHeaders)
+            .pipe(map(response => <OUT>response.json()));
     }
 
     get<T>(url: string): Observable<T> {
         return this._http
-            .get(this.buildUrl(url), this.createHeaders)
-            .pipe(map((response) => <T>response.json()));
+            .get(buildUrl(url), this.createHeaders)
+            .pipe(map(response => <T>response.json()));
     }
 
     delete<T>(url: string): Observable<T> {
         return this._http
-            .delete(this.buildUrl(url), this.createHeaders)
-            .pipe(map((response) => <T>response.json()));
+            .delete(buildUrl(url), this.createHeaders)
+            .pipe(map(response => <T>response.json()));
     }
 
     get createHeaders(): RequestOptionsArgs {
@@ -37,14 +37,16 @@ export class HttpService {
         if (!!this._auth.accessToken) {
             headers.append('Authorization', 'Bearer ' + this._auth.accessToken);
         }
-        return {headers: headers};
+        return { headers: headers };
     }
+}
 
-    private buildUrl(url:string) {
-        if (url.startsWith('/')) {
-            return url.substr(1);
-        } else {
-            return url;
-        }
+function buildUrl(url: string): string {
+    let u = '';
+    if (url.startsWith('/')) {
+        u = url.substr(1);
+    } else {
+        u = url;
     }
+    return environment.baseUrl + '/' + u;
 }
