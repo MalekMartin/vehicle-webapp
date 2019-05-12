6ffb0cf868621e619aefe5262a08acacbd33e966
MalekMartin <malek.martin@seznam.cz>
2019-05-12 07:36:19 +0200

    Removed SharedModules from TechnicalnspectionModule.

--- ui/src/app/vehicles/vehicle-detail/technical-inspection/technical-inspection.module.ts
+++ ui/src/app/vehicles/vehicle-detail/technical-inspection/technical-inspection.module.ts
@@ -1,36 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { TechnicalInspectionComponent } from './technical-inspection.component';
-import { InspectionFormComponent } from './inspection-form/inspection-form.component';
-import { SharedModule } from '../../../shared/shared.module';
-import { StationFormComponent } from './station-form/station-form.component';
-import { StationCardComponent } from './station-card/station-card.component';
-import { InspectionCardComponent } from './inspection-card/inspection-card.component';
-import { ProgressbarModule } from 'ngx-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatSelectModule
+} from '@angular/material';
 import { RouterModule } from '@angular/router';
+import { CardModule } from '../../../shared/components/card/card.module';
+import { ConfirmModule } from '../../../shared/components/confirm/confirm.module';
+import { DatepickerModule } from '../../../shared/components/datepicker/datepicker.module';
+import { PaginatorModule } from '../../../shared/components/paginator/paginator.module';
+import { ProgressModule } from '../../../shared/components/progress/progress.module';
+import { SectionHeadingModule } from '../../../shared/components/section-heading/section-heading.module';
+import { SpinnerModule } from '../../../shared/components/spinner/spinner.module';
+import { PipesModule } from '../../../shared/pipes/pipes.module';
+import { InspectionAddComponent } from './inspection-add/inspection-add.component';
+import { InspectionCardComponent } from './inspection-card/inspection-card.component';
+import { InspectionEditComponent } from './inspection-edit/inspection-edit.component';
+import { InspectionFormComponent } from './inspection-form/inspection-form.component';
+import { StationAddComponent } from './station-add/station-add.component';
+import { StationCardComponent } from './station-card/station-card.component';
+import { StationEditComponent } from './station-edit/station-edit.component';
+import { StationFormComponent } from './station-form/station-form.component';
+import { TechnicalInspectionComponent } from './technical-inspection.component';
 import { technicalRoutes } from './technical-inspection.routes';
 import { TechnicalInspectionService } from './technical-inspection.service';
-import { DatepickerModule } from '../../../shared/components/datepicker/datepicker.module';
-import { InspectionAddComponent } from './inspection-add/inspection-add.component';
-import { InspectionEditComponent } from './inspection-edit/inspection-edit.component';
-import {
-    MatDialogModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSelectModule,
-    MatInputModule,
-    MatCheckboxModule
-} from '@angular/material';
-import { ConfirmModule } from '../../../shared/components/confirm/confirm.module';
-import { SpinnerModule } from '../../../shared/components/spinner/spinner.module';
-import { StationAddComponent } from './station-add/station-add.component';
-import { StationEditComponent } from './station-edit/station-edit.component';
 
 @NgModule({
     imports: [
-        SharedModule,
-        ProgressbarModule,
+        CommonModule,
+        ReactiveFormsModule,
+        SectionHeadingModule,
+        PaginatorModule,
+        CardModule,
+        PipesModule,
+        ProgressModule,
         RouterModule.forChild(technicalRoutes),
         DatepickerModule,
         MatDialogModule,
