687b2a1e74307eb8718d4662ca4260ebe092c367
MalekMartin <malek.martin@seznam.cz>
2019-05-12 07:19:17 +0200

    Updated VehicleSettings.

--- ui/src/app/vehicles/vehicle-detail/vehicle-settings/vehicle-settings.module.ts
+++ ui/src/app/vehicles/vehicle-detail/vehicle-settings/vehicle-settings.module.ts
@@ -2,20 +2,37 @@ import { NgModule } from '@angular/core';
 
 import { VehicleSettingsComponent } from './vehicle-settings.component';
 import { FileUploadModule } from 'ng2-file-upload';
-import { SharedModule } from '../../../shared/shared.module';
 import { SettingsService } from './settings.service';
 import { CommonModule } from '@angular/common';
 import { SettingsFormComponent } from './settings-form/settings-form.component';
+import { SectionHeadingModule } from '../../../shared/components/section-heading/section-heading.module';
+import { ModalModule } from 'ngx-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormItemModule } from '../../../shared/forms/form-item/form-item.module';
+import {
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule
+} from '@angular/material';
 
-const COMPONENTS = [
-    VehicleSettingsComponent,
-    SettingsFormComponent
-];
+const COMPONENTS = [VehicleSettingsComponent, SettingsFormComponent];
+
+const MAT_MODULES = [MatFormFieldModule, MatDialogModule, MatInputModule, MatButtonModule];
 
 @NgModule({
-    imports: [SharedModule],
+    imports: [
+        CommonModule,
+        SectionHeadingModule,
+        ModalModule,
+        ReactiveFormsModule,
+        FormItemModule,
+        FileUploadModule,
+        ...MAT_MODULES
+    ],
     exports: [...COMPONENTS],
     declarations: [...COMPONENTS],
     providers: [SettingsService],
+    entryComponents: [SettingsFormComponent]
 })
-export class VehicleSettingsModule { }
+export class VehicleSettingsModule {}
