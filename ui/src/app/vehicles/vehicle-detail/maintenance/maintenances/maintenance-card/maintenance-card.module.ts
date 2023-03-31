9f956367093a6bb8afc52cd01fb5688328b8f86b
malek.martin@seznam.cz <M86k379DT>
2023-03-31 06:31:57 +0200

    Replaced material legacy modules with the new ones in maintenance-card module.

--- ui/src/app/vehicles/vehicle-detail/maintenance/maintenances/maintenance-card/maintenance-card.module.ts
+++ ui/src/app/vehicles/vehicle-detail/maintenance/maintenances/maintenance-card/maintenance-card.module.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
-import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { CardModule } from '../../../../../shared/components/card/card.module';
 import { ProgressModule } from '../../../../../shared/components/progress/progress.module';
