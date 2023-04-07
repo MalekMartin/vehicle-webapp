2f26c41a6a61a44b76f877653974bee24f7594ad
malek.martin@seznam.cz <M86k379DT>
2023-04-07 22:36:13 +0200

    Updated main navigation.

--- ui/src/app/vehicles/vehicle-detail/vehicle-detail-menu/vehicle-detail-menu.component.ts
+++ ui/src/app/vehicles/vehicle-detail/vehicle-detail-menu/vehicle-detail-menu.component.ts
@@ -1,24 +1,31 @@
-import { Component, Input } from '@angular/core';
-import { MenuOption } from './vehicle-detail-menu.interface';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { MenuOption } from "./vehicle-detail-menu.interface";
 
 @Component({
-    selector: 'va-vehicle-detail-menu',
-    templateUrl: 'vehicle-detail-menu.component.html',
-    styleUrls: ['./vehicle-detail-menu.component.scss']
+    selector: "va-vehicle-detail-menu",
+    templateUrl: "vehicle-detail-menu.component.html",
+    styleUrls: ["./vehicle-detail-menu.component.scss"],
 })
 export class VehicleDetailMenuComponent {
     @Input() vehicleId: string;
 
+    @Output() navigated = new EventEmitter<void>();
+
     options: MenuOption[] = [
-        { page: 'dashboard', label: 'Přehled', warning: 0, icon: 'stats' },
-        { page: 'info', label: 'Info', warning: 0, icon: 'info' },
-        { page: 'fuel', label: 'Tankování', warning: 0, icon: 'gas' },
-        { page: 'costs', label: 'Náklady', warning: 0, icon: 'money' },
-        { page: 'tires', label: 'Pneu', warning: 0, icon: 'radio-unchecked' },
-        { page: 'technical', label: 'TK', warning: 0, icon: 'verified' },
-        { page: 'maintenance', label: 'Údržba', warning: 0, icon: 'wrench' },
-        { page: 'repairs', label: 'Servisí práce', warning: 0, icon: 'automobile' },
-        { page: 'manuals', label: 'Manuály', warning: 0, icon: 'database' },
-        { page: 'settings', label: 'Nastavení', warning: 0, icon: 'settings' }
+        { page: "dashboard", label: "Přehled", warning: 0, icon: "stats" },
+        { page: "info", label: "Info", warning: 0, icon: "info" },
+        { page: "fuel", label: "Tankování", warning: 0, icon: "gas" },
+        { page: "costs", label: "Náklady", warning: 0, icon: "money" },
+        { page: "tires", label: "Pneu", warning: 0, icon: "radio-unchecked" },
+        { page: "technical", label: "TK", warning: 0, icon: "verified" },
+        { page: "maintenance", label: "Údržba", warning: 0, icon: "wrench" },
+        {
+            page: "repairs",
+            label: "Servisí práce",
+            warning: 0,
+            icon: "automobile",
+        },
+        { page: "manuals", label: "Manuály", warning: 0, icon: "database" },
+        { page: "settings", label: "Nastavení", warning: 0, icon: "settings" },
     ];
 }
